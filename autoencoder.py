1d2720545c7bebef136812d40afce37555463f5f
Yang Liu <foamliu@yeah.net>
2018-05-06 17:32:02 +0800

    尝试可以在多大程度上改动模型同时还可以加载 pre-trained VGG16

--- autoencoder.py
+++ autoencoder.py
@@ -1,16 +1,16 @@
 from keras.models import Sequential
 from keras.optimizers import SGD
-from keras.layers import Input, Dense, Conv2D, MaxPooling2D, UpSampling2D, ZeroPadding2D, Dropout, Flatten, merge, \
-    Reshape, Activation
+from keras.layers import Conv2D, MaxPooling2D, UpSampling2D, ZeroPadding2D
 from utils import custom_loss
 
 
-def autoencoder(img_rows, img_cols, channel=1, num_classes=None):
+def autoencoder(img_rows, img_cols, channel=1):
     model = Sequential()
+    # Encoder
     model.add(ZeroPadding2D((1, 1), input_shape=(img_rows, img_cols, channel)))
-    model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(Conv2D(64, (3, 3), activation='relu', name='conv1_1'))
     model.add(ZeroPadding2D((1, 1)))
-    model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(Conv2D(64, (3, 3), activation='relu', name='conv1_2'))
     model.add(MaxPooling2D((2, 2), strides=(2, 2)))
 
     model.add(ZeroPadding2D((1, 1)))
@@ -43,22 +43,29 @@ def autoencoder(img_rows, img_cols, channel=1, num_classes=None):
     model.add(Conv2D(512, (3, 3), activation='relu'))
     model.add(MaxPooling2D((2, 2), strides=(2, 2)))
 
+    # Decoder
     model.add(Conv2D(512, (1, 1), activation='relu', padding='same', name='deconv6'))
     model.add(UpSampling2D(size=(2, 2)))
+
     model.add(Conv2D(512, (5, 5), activation='relu', padding='same', name='deconv5_1'))
     model.add(Conv2D(512, (5, 5), activation='relu', padding='same', name='deconv5_2'))
     model.add(UpSampling2D(size=(2, 2)))
+
     model.add(Conv2D(256, (5, 5), activation='relu', padding='same', name='deconv4_1'))
     model.add(Conv2D(256, (5, 5), activation='relu', padding='same', name='deconv4_2'))
     model.add(UpSampling2D(size=(2, 2)))
+
     model.add(Conv2D(128, (5, 5), activation='relu', padding='same', name='deconv3_1'))
     model.add(Conv2D(128, (5, 5), activation='relu', padding='same', name='deconv3_2'))
     model.add(UpSampling2D(size=(2, 2)))
+
     model.add(Conv2D(64, (5, 5), activation='relu', padding='same', name='deconv2_1'))
     model.add(Conv2D(64, (5, 5), activation='relu', padding='same', name='deconv2_2'))
     model.add(UpSampling2D(size=(2, 2)))
+
     model.add(Conv2D(64, (5, 5), activation='relu', padding='same', name='deconv1_1'))
     model.add(Conv2D(64, (5, 5), activation='relu', padding='same', name='deconv1_2'))
+
     model.add(Conv2D(1, (5, 5), activation='sigmoid', padding='same', name='pred'))
 
     # Learning rate is changed to 0.001
