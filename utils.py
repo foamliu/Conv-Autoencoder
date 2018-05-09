Fix BUG: 'could not broadcast input array from shape (224,224) into shape (320,320)'

utils.py
@@ -35,12 +35,12 @@ def load_data():
             rgb_img = cv.cvtColor(bgr_img, cv.COLOR_BGR2RGB)
             if filename.startswith('data/train'):
                 x_train[i_train, :, :, 0:3] = rgb_img / 255.
-                x_train[i_train, :, :, 3] = np.random.uniform(0, 1, (224, 224))
+                x_train[i_train, :, :, 3] = np.random.uniform(0, 1, (320, 320))
                 y_train[i_train, :, :, 0] = gray_img / 255.
                 i_train += 1
             else:
                 x_valid[i_valid, :, :, 0:3] = rgb_img / 255.
-                x_valid[i_train, :, :, 3] = np.random.uniform(0, 1, (224, 224))
+                x_valid[i_train, :, :, 3] = np.random.uniform(0, 1, (320, 320))
                 y_valid[i_valid, :, :, 0] = gray_img / 255.
                 i_valid += 1
 