25a44d507c07e8a36f650ce75b1aa589e051b669
Yang Liu <foamliu@yeah.net>
2018-05-11 17:23:31 +0800

    正在 MNIST 上通过一个极简单的autoencoder 调试 unpolling

--- unpolling.py
+++ unpolling.py
@@ -1,37 +1,49 @@
-import keras
 import keras.backend as K
-from keras.layers import Input, Conv2D, UpSampling2D, Reshape, Permute, Activation, MaxPooling2D
+import numpy as np
+from keras.datasets import mnist
+from keras.layers import Input, Conv2D, UpSampling2D, MaxPooling2D
 from keras.models import Model
 
+
+def custom_loss(y_true, y_pred):
+    epsilon = 1e-6
+    epsilon_sqr = K.constant(epsilon ** 2)
+    return K.mean(K.sqrt(K.square(y_pred - y_true) + epsilon_sqr))
+
+
 if __name__ == '__main__':
-    h_image = w_image = 28
-    channel = 1
-    n_classes = 10
-    img_input = Input(shape=(h_image, w_image, channel))
-    x = Conv2D(512, (3, 3), padding='same')(img_input)
-    orig = x  # Save output x
-    x = MaxPooling2D()(x)
+    input_img = Input(shape=(28, 28, 1))
 
-    x = UpSampling2D()(x)
+    x = Conv2D(16, (3, 3), activation='relu', padding='same')(input_img)
 
-    model = Model(inputs=img_input, outputs=x)
+    x = MaxPooling2D((2, 2), padding='same')(x)
+    x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
+    x = MaxPooling2D((2, 2), padding='same')(x)
+    x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
+    encoded = MaxPooling2D((2, 2), padding='same')(x)
 
-    print(model.summary())
+    x = Conv2D(8, (3, 3), activation='relu', padding='same')(encoded)
+    x = UpSampling2D((2, 2))(x)
+    x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
+    x = UpSampling2D((2, 2))(x)
+    x = Conv2D(16, (3, 3), activation='relu')(x)
+    x = UpSampling2D((2, 2))(x)
+    decoded = Conv2D(1, (3, 3), activation='sigmoid', padding='same')(x)
 
-    bool_mask = K.greater_equal(orig, x)
-    mask = K.cast(bool_mask, dtype='float32')
+    autoencoder = Model(input_img, decoded)
+    encoder = Model(input_img, encoded)
 
-    mask_input = Input(tensor=mask)  # Makes the mask to a Keras tensor to use as input
-    x = keras.layers.multiply([mask_input, x])
-    x = Conv2D(512, (3, 3), padding='same')(x)
-    model = Model(inputs=img_input, outputs=x)
+    autoencoder.compile(optimizer='adam', loss=custom_loss)
 
-    print(model.summary())
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-    x = Reshape((n_classes, w_image * h_image))(x)
-    x = Permute((2, 1))(x)
-    main_output = Activation('softmax')(x)
+    x_train = np.reshape(x_train, (-1, 28, 28, 1))
+    x_train = x_train / 255.
 
-    model = Model(inputs=img_input, outputs=main_output)
+    print(autoencoder.summary())
 
-    print(model.summary())
+    autoencoder.fit(x_train, x_train,
+                    epochs=50,
+                    batch_size=128,
+                    shuffle=True,
+                    verbose=1)
