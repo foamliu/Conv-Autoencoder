b6fae10d634472e341e5de6e7fdc5d4abd9acfc1
Yang Liu <foamliu@yeah.net>
2018-05-16 00:00:06 +0800

    resolve conflicts

--- train.py
+++ train.py
@@ -1,34 +1,18 @@
 import keras
 from keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau
 
-<<<<<<< HEAD
 import migrate
 from model import create_model
-=======
-import new_start
-import migrate
->>>>>>> 66b86b071e4d5e7bc9b3452559133c54ae30d1bf
 from utils import load_data
 
 if __name__ == '__main__':
-    img_rows, img_cols = 320, 320
-    channel = 4
     batch_size = 16
     epochs = 1000
     patience = 50
 
     # Load our model
-<<<<<<< HEAD
     model = create_model()
     migrate.migrate_model(model)
-=======
-    model_path = 'models/model_weights.h5'
-    if os.path.exists(model_path):
-        model = new_start.autoencoder(img_rows, img_cols, channel)
-        model.load_weights(model_path)
-    else:
-        model = migrate.migrate_model(img_rows, img_cols, channel)
->>>>>>> 66b86b071e4d5e7bc9b3452559133c54ae30d1bf
 
     print(model.summary())
 
@@ -38,7 +22,7 @@ if __name__ == '__main__':
     # Callbacks
     tensor_board = keras.callbacks.TensorBoard(log_dir='./logs', histogram_freq=0, write_graph=True, write_images=True)
     trained_models_path = 'models/model'
-    model_names = trained_models_path + '.{epoch:02d}-{val_loss:.2f}.hdf5'
+    model_names = trained_models_path + '.{epoch:02d}-{val_loss:.4f}.hdf5'
     model_checkpoint = ModelCheckpoint(model_names, monitor='val_loss', verbose=1, save_best_only=True)
     early_stop = EarlyStopping('val_loss', patience=patience)
     reduce_lr = ReduceLROnPlateau('val_loss', factor=0.1, patience=int(patience / 4), verbose=1)
