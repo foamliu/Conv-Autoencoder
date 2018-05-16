e269c0baed3ff041843f76d7530687e4ece60d1c
Yang Liu <foamliu@yeah.net>
2018-05-16 12:31:47 +0800

    update readme and images

--- demo.py
+++ demo.py
@@ -1,4 +1,5 @@
-import argparse
+import os
+import random
 
 import cv2 as cv
 import keras.backend as K
@@ -10,43 +11,41 @@ if __name__ == '__main__':
     img_rows, img_cols = 320, 320
     channel = 4
 
-    model_weights_path = 'models/model.362-0.06.hdf5'
+    model_weights_path = 'models/model.21-0.0330.hdf5'
     model = create_model()
 
     model.load_weights(model_weights_path)
     print(model.summary())
 
-    ap = argparse.ArgumentParser()
-    ap.add_argument("-i", "--image", help="path to the image file")
-    args = vars(ap.parse_args())
-    filename = args["image"]
-    if filename is None:
-        car_ids = ['00133', '06258', '07602']
-        for car_id in car_ids:
-            filename = 'images/{}.jpg'.format(car_id)
+    test_path = 'data/test/'
+    test_images = [f for f in os.listdir(test_path) if
+                   os.path.isfile(os.path.join(test_path, f)) and f.endswith('.jpg')]
 
-            print('Start processing image: {}'.format(filename))
+    samples = random.sample(test_images, 10)
 
-            x_test = np.empty((1, img_rows, img_cols, 4), dtype=np.float32)
-            bgr_img = cv.imread(filename)
-            gray_img = cv.cvtColor(bgr_img, cv.COLOR_BGR2GRAY)
-            rgb_img = cv.cvtColor(bgr_img, cv.COLOR_BGR2RGB)
-            rgb_img = rgb_img / 255.
-            x_test[0, :, :, 0:3] = rgb_img
-            x_test[0, :, :, 3] = np.random.uniform(0, 1, (img_rows, img_cols))
-            # rgb_img = np.expand_dims(rgb_img, 0)
+    for i in range(len(samples)):
+        image_name = samples[i]
+        filename = os.path.join(test_path, image_name)
 
-            rep = model.predict(x_test)
-            print(rep.shape)
+        print('Start processing image: {}'.format(filename))
 
-            rep = np.reshape(rep, (img_rows, img_cols))
-            rep = rep * 255.0
-            rep = rep.astype(np.uint8)
-            print(rep)
-            cv.imshow('rep', rep)
-            cv.imwrite('images/{}_out.jpg'.format(car_id), rep)
-            cv.imwrite('images/{}_gray.jpg'.format(car_id), gray_img)
-            cv.waitKey(0)
-            cv.destroyAllWindows()
+        x_test = np.empty((1, img_rows, img_cols, 4), dtype=np.float32)
+        bgr_img = cv.imread(filename)
+        gray_img = cv.cvtColor(bgr_img, cv.COLOR_BGR2GRAY)
+        rgb_img = cv.cvtColor(bgr_img, cv.COLOR_BGR2RGB)
+        rgb_img = rgb_img / 255.
+        x_test[0, :, :, 0:3] = rgb_img
+        x_test[0, :, :, 3] = np.random.uniform(0, 1, (img_rows, img_cols))
+
+        out = model.predict(x_test)
+        # print(out.shape)
+
+        out = np.reshape(out, (img_rows, img_cols))
+        out = out * 255.0
+        out = out.astype(np.uint8)
+
+        cv.imwrite('images/{}_image.png'.format(i), bgr_img)
+        cv.imwrite('images/{}_out.png'.format(i), out)
+        cv.imwrite('images/{}_gray.png'.format(i), gray_img)
 
     K.clear_session()
